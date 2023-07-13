2bbdf531cd19c3a2b70849ccd7690d65391e7063
Anupam Mandal <95023203+Anupam890@users.noreply.github.com>
2023-07-13 15:21:16 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+const github = document.getElementById('github');
+const instagram = document.getElementById('instagram')
+const linkedin = document.getElementById('linkedin')
+
+github.addEventListener('click', () => {
+    window.open('https://github.com/Anupam890');
+})
+
+// instagram.addEventListener('click', () => {
+//     window.open('')
+
+// })
+linkedin.addEventListener('click', () => {
+    window.open('https://www.linkedin.com/in/anupam-mandal-536774247/')
+
+})
