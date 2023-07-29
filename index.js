1b0a30269a0cc101144bd5b753a207f0a22d5f49
Anupam Mandal <95023203+Anupam890@users.noreply.github.com>
2023-07-29 14:46:24 +0530

    links added

--- index.js
+++ index.js
@@ -16,19 +16,3 @@ linkedin.addEventListener('click', () => {
 
 })
 
-// const scriptURL = 'https://script.google.com/macros/s/AKfycbzGRpSy_nb7jny8yMJOg7M9_YYJNqlbu-Pozd2RDcgsZwMmvBZt0fZYXwGlefKtJA8M/exec'
-// const form = document.forms['submit-to-google-sheet']
-// const msg = document.getElementById("msg")
-//
-// form.addEventListener('submit', e => {
-//     e.preventDefault()
-//     fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-//         .then(response => {
-//             msg.innerHTML = "Message sent successfully"
-//             setTimeout(function(){
-//                 mag.innerHTML = ""
-//             },5000)
-//             form.reset()
-//         })
-//         .catch(error => console.error('Error!', error.message))
-// })
